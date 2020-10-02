8f36ba08c6551bb7d895e12a6fc29a884c639cca
Yaron <yaron.mic@gmail.com>
2020-10-03 01:19:12 +0300

    Uses compression

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const compression = require('compression');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const app = express()
+const app = express();
+
 
 app.use(express.static('dist'));
-
+app.use(compression());
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
