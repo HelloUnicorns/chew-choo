8ceaed4f6d757c0080a8a53c1875df062116a789
Yaron <yaron.mic@gmail.com>
2020-10-03 11:43:12 +0300

    Drawing track

--- client/game.js
+++ client/game.js
@@ -1,87 +1,53 @@
 const Phaser = require('phaser');
 const { send_event, event_handlers } = require('./websockets.js');
+
+const TRACK_PIECE_WIDTH = 100;
+const TRACK_WIDTH = 10;
+const TRACK_HEIGHT = 5;
 let game_inited = false;
-let emitter;
 let client_id;
-let emitter_tint;
-let other_players_emitters = {};
-let particles;
 
 function preload() {
-    this.load.image('sky', 'assets/space3.png');
-    this.load.image('logo', 'assets/phaser3-logo.png');
-    this.load.image('particle', 'assets/particle.png');
+    this.load.image('track', 'assets/track.png');
+    this.load.image('turn', 'assets/turn.png');
 }
 
-function create_trail() {
-    return particles.createEmitter({
-        speed: 100,
-        scale: { start: 0.3, end: 0 },
-        blendMode: 'ADD'
-    });
+function draw_track_piece(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
+    let track = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'track');
+    track.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
+}
+
+function draw_corner_piece(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
+    let track = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'turn');
+    track.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
+}
+
+function draw_tracks(scene, origin_x, origin_y) {
+    for (let i = 1; i < TRACK_WIDTH - 1; i++) {
+        draw_track_piece(scene, origin_x, origin_y, i, 0, 0);
+        draw_track_piece(scene, origin_x, origin_y, i, TRACK_HEIGHT - 1, 180);
+    }
+    for (let i = 1; i < TRACK_HEIGHT - 1; i++) {
+        draw_track_piece(scene, origin_x, origin_y, 0, i, 270);
+        draw_track_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, i, 90);
+    }
+    draw_corner_piece(scene, origin_x, origin_y, 0, 0, 0);
+    draw_corner_piece(scene, origin_x, origin_y, 0, TRACK_HEIGHT - 1, 270);
+    draw_corner_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, 0, 90);
+    draw_corner_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, TRACK_HEIGHT - 1, 180);
 }
 
 function create() {
-    this.add.image(400, 300, 'sky');
-
-    particles = this.add.particles('particle');
-    emitter = create_trail();
-    
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setGravity(0, 100);
-    logo.setCollideWorldBounds(true);
-
-    if (emitter_tint) {
-        emitter.setTint(emitter_tint);
-    }
-    this.input.on('pointerdown', click_handler);
+    draw_tracks(this, 100, 100);
     game_inited = true;
 }
 
-function click_handler() {
-    send_event({type: 'switch_tint'});
-}
-
 function update() {
-    emitter.setPosition(game.input.mousePointer.x, game.input.mousePointer.y);
-    send_event({type: 'location', x: game.input.mousePointer.x, y: game.input.mousePointer.y});
+
 }
-
-event_handlers.locations = (event) => {
-    if (!game_inited) {
-        return;
-    }
-    let old_other_players_emitters = Object.assign({}, other_players_emitters);
-    let new_other_players_emitters = {};
-
-    event.location_info.forEach((info) => {
-        if (info.client_id == client_id) {
-            emitter.setTint(info.tint);
-            return;
-        }
-        new_other_players_emitters[info.client_id] = old_other_players_emitters[info.client_id] || create_trail();
-        new_other_players_emitters[info.client_id].setPosition(info.x, info.y);
-        new_other_players_emitters[info.client_id].setTint(info.tint);
-        delete old_other_players_emitters[info.client_id];
-    });
-
-    other_players_emitters = new_other_players_emitters;
-
-    /* those who were not deleted are players that left */
-    for (client_id in old_other_players_emitters) {
-        old_other_players_emitters[client_id].remove();
-    }
-};
 
 event_handlers.connection = (event) => {
     client_id = event.client_id;
-    emitter_tint = event.tint;
-    if (game_inited) {
-        emitter.setTint(emitter_tint);
-    }
 };
 
 const game = new Phaser.Game({
