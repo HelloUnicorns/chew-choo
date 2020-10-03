89212506e2b6e2362949c1d3b20c5aee577a209c
Ido Weinstein <ido.weins@gmail.com>
2020-10-03 12:54:53 +0300

    Added car placement

--- client/game.js
+++ client/game.js
@@ -10,6 +10,7 @@ let client_id;
 function preload() {
     this.load.image('track', 'assets/track.png');
     this.load.image('turn', 'assets/turn.png');
+    this.load.image('car', 'assests/car.jpg')
 }
 
 function draw_track_piece(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
@@ -20,6 +21,10 @@ function draw_track_piece(scene, origin_x, origin_y, index_x, index_y, rotation_
 function draw_corner_piece(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
     let track = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'turn');
     track.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
+}
+
+function draw_car(scene, origin_x, origin_y, index_x, index_y) {
+    let car = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'car');
 }
 
 function draw_tracks(scene, origin_x, origin_y) {
@@ -37,8 +42,32 @@ function draw_tracks(scene, origin_x, origin_y) {
     draw_corner_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, TRACK_HEIGHT - 1, 180);
 }
 
+function place_car(scene, origin_x, origin_y) {
+    /* Select start position */
+    let amount_of_tracks = (TRACK_HEIGHT + TRACK_WIDTH) * 2 - 4;
+    let selected_track = Math.floor(Math.random() * amount_of_tracks);
+    let position = undefined;
+    if (selected_track < TRACK_WIDTH) {
+        /* First row */
+        position = [selected_track, 0];
+    } else if (selected_track + TRACK_WIDTH > amount_of_tracks) {
+        /* Last row */
+        position = [amount_of_tracks - selected_track, TRACK_HEIGHT - 1]; 
+    } else {
+        /* Middle rows */
+        position = [
+            (selected_track % 2) * (TRACK_WIDTH - 1),
+            Math.floor((selected_track - TRACK_WIDTH) / 2)
+        ];
+    }
+    console.log(selected_track);
+    console.log(position);
+    draw_car(scene, origin_x, origin_y, position[0], position[1]);
+}
+
 function create() {
     draw_tracks(this, 100, 100);
+    place_car(this, 100, 100);
     game_inited = true;
 }
 
