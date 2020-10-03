5dd4fcecc3a9021e69b6f325e50d9f006bcad91b
Yaron <yaron.mic@gmail.com>
2020-10-03 14:25:21 +0300

    Changed cart

--- client/game.js
+++ client/game.js
@@ -1,99 +1,27 @@
 const Phaser = require('phaser');
 const { send_event, event_handlers } = require('./websockets.js');
 
-const TRACK_PIECE_IMAGE_WIDTH = 100;
+const GRID_ORIGIN_X = 40;
+const GRID_ORIGIN_Y = 40;
+
+const GRID_PIECE_IMAGE_WIDTH = 100;
 const TRACK_SCALE = 0.3;
-const TRACK_PIECE_WIDTH = TRACK_PIECE_IMAGE_WIDTH * TRACK_SCALE;
+const GRID_PIECE_WIDTH = GRID_PIECE_IMAGE_WIDTH * TRACK_SCALE;
 const TRACK_WIDTH = 30;
 const TRACK_HEIGHT = 20;
+const TRACK_AMOUNT = (TRACK_HEIGHT + TRACK_WIDTH) * 2 - 4;
 
 const CART_IMAGE_WIDTH = 100;
-const CART_WIDTH = TRACK_PIECE_WIDTH;
+const CART_WIDTH = GRID_PIECE_WIDTH;
 const CART_SCALE = CART_WIDTH / CART_IMAGE_WIDTH;
-
 
 let game_inited = false;
 let client_id;
-
-function preload() {
-    this.load.image('track', 'assets/track.png');
-    this.load.image('turn', 'assets/turn.png');
-    this.load.image('cart', 'assets/cart.png')
+let player = {
+    car_sprite: undefined,
+    car_grid_index: 0
 }
 
-function draw_track_piece(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
-    let track = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'track');
-    track.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
-    track.setScale(TRACK_SCALE);
-}
-
-function draw_corner_piece(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
-    let track = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'turn');
-    track.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
-    track.setScale(TRACK_SCALE);
-}
-
-function draw_cart(scene, origin_x, origin_y, index_x, index_y, rotation_degrees) {
-    let cart = scene.add.sprite(origin_x + index_x * TRACK_PIECE_WIDTH, origin_y + index_y * TRACK_PIECE_WIDTH, 'cart');
-    cart.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
-    cart.setScale(CART_SCALE);
-}
-
-function draw_tracks(scene, origin_x, origin_y) {
-    for (let i = 1; i < TRACK_WIDTH - 1; i++) {
-        draw_track_piece(scene, origin_x, origin_y, i, 0, 0);
-        draw_track_piece(scene, origin_x, origin_y, i, TRACK_HEIGHT - 1, 180);
-    }
-    for (let i = 1; i < TRACK_HEIGHT - 1; i++) {
-        draw_track_piece(scene, origin_x, origin_y, 0, i, 270);
-        draw_track_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, i, 90);
-    }
-    draw_corner_piece(scene, origin_x, origin_y, 0, 0, 270);
-    draw_corner_piece(scene, origin_x, origin_y, 0, TRACK_HEIGHT - 1, 180);
-    draw_corner_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, 0, 0);
-    draw_corner_piece(scene, origin_x, origin_y, TRACK_WIDTH - 1, TRACK_HEIGHT - 1, 90);
-}
-
-function place_car(scene, origin_x, origin_y) {
-    /* Select start position */
-    let amount_of_tracks = (TRACK_HEIGHT + TRACK_WIDTH) * 2 - 4;
-    let selected_track = Math.floor(Math.random() * amount_of_tracks);
-    let position = undefined;
-    if (selected_track < TRACK_WIDTH) {
-        /* First row */
-        position = [selected_track, 0];
-        rotation_degrees = 0;
-    } else if (selected_track + TRACK_WIDTH > amount_of_tracks) {
-        /* Last row */
-        position = [amount_of_tracks - selected_track, TRACK_HEIGHT - 1];
-        rotation_degrees = 180;
-    } else {
-        /* Middle rows */
-        position = [
-            (selected_track % 2) * (TRACK_WIDTH - 1),
-            Math.floor((selected_track - TRACK_WIDTH) / 2)
-        ];
-        rotation_degrees = 270 - 180 * (selected_track % 2);
-    }
-    console.log(selected_track);
-    console.log(position);
-    draw_cart(scene, origin_x, origin_y, position[0], position[1], rotation_degrees);
-}
-
-function create() {
-    this.cameras.main.setBackgroundColor(0xf7f1da)
-    draw_tracks(this, 100, 100);
-    place_car(this, 100, 100);
-    game_inited = true;
-}
-
-function update() {
-
-}
-
-event_handlers.connection = (event) => {
-    client_id = event.client_id;
-};
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
@@ -113,3 +41,98 @@ const game = new Phaser.Game({
     },
     
 });
+
+function preload() {
+    this.load.image('track', 'assets/track.png');
+    this.load.image('turn', 'assets/turn.png');
+    this.load.image('cart', 'assets/cart.png')
+}
+
+function update_grid_sprite(sprite, grid_x, grid_y, rotation_degrees) {
+    sprite.setPosition(GRID_ORIGIN_X + grid_x * GRID_PIECE_WIDTH, GRID_ORIGIN_Y + grid_y * GRID_PIECE_WIDTH);
+    sprite.setRotation(rotation_degrees * Phaser.Math.DEG_TO_RAD);
+}
+
+function draw_grid_sprite(scene, grid_x, grid_y, rotation_degrees, sprite_name, scale) {
+    let grid_sprite = scene.add.sprite(0, 0, sprite_name);
+    update_grid_sprite(grid_sprite, grid_x, grid_y, rotation_degrees);
+    grid_sprite.setScale(scale);
+    return grid_sprite;
+}
+
+function draw_track_piece(scene, grid_x, grid_y, rotation_degrees) {
+    return draw_grid_sprite(scene, grid_x, grid_y, rotation_degrees, 'track', TRACK_SCALE);
+}
+
+function draw_corner_piece(scene, grid_x, grid_y, rotation_degrees) {
+    return draw_grid_sprite(scene, grid_x, grid_y, rotation_degrees, 'turn', TRACK_SCALE);
+}
+
+function draw_cart(scene, grid_x, grid_y, rotation_degrees) {
+    return draw_grid_sprite(scene, grid_x, grid_y, rotation_degrees, 'cart', CART_SCALE);
+}
+
+function draw_tracks(scene) {
+    for (let i = 1; i < TRACK_WIDTH - 1; i++) {
+        draw_track_piece(scene, i, 0, 0);
+        draw_track_piece(scene, i, TRACK_HEIGHT - 1, 180);
+    }
+    for (let i = 1; i < TRACK_HEIGHT - 1; i++) {
+        draw_track_piece(scene, 0, i, 270);
+        draw_track_piece(scene, TRACK_WIDTH - 1, i, 90);
+    }
+    draw_corner_piece(scene, 0, 0, 270);
+    draw_corner_piece(scene, 0, TRACK_HEIGHT - 1, 180);
+    draw_corner_piece(scene, TRACK_WIDTH - 1, 0, 0);
+    draw_corner_piece(scene, TRACK_WIDTH - 1, TRACK_HEIGHT - 1, 90);
+}
+
+function track_id_to_grid_index(track_id) {
+    if (track_id < TRACK_WIDTH - 1) {
+        /* top row */
+        return { x: track_id, y: 0, rotation_degrees: 0};
+    } 
+    track_id -= TRACK_WIDTH - 1;
+
+    if (track_id < TRACK_HEIGHT - 1) {
+        /* right culomn */
+        return { x: TRACK_WIDTH - 1, y: track_id, rotation_degrees: 90 };
+    }
+    track_id -= TRACK_HEIGHT - 1;
+    
+    if (track_id < TRACK_WIDTH - 1) {
+        /* bottom row */
+        return { x: TRACK_WIDTH - 1 - track_id, y: TRACK_HEIGHT - 1, rotation_degrees: 180 };
+    }
+    track_id -= TRACK_WIDTH - 1;
+
+    /* left culomn */
+    return { x: 0, y: TRACK_HEIGHT - 1 - track_id, rotation_degrees: 270 };
+}
+
+function place_car(scene) {
+    grid_index = track_id_to_grid_index(player.car_grid_index);
+    player.cart_sprite = draw_cart(scene, grid_index.x, grid_index.y, grid_index.rotation_degrees);
+}
+
+function create() {
+    this.cameras.main.setBackgroundColor(0xf7f1da);
+    draw_tracks(this);
+    place_car(this);
+    game_inited = true;
+    this.time.addEvent({ delay: 1000 / 10, callback: advance_track, callbackScope: this, loop: true });
+}
+
+function advance_track() {
+    player.car_grid_index++;
+    player.car_grid_index %= TRACK_AMOUNT;
+}
+
+function update() {
+    grid_index = track_id_to_grid_index(player.car_grid_index);
+    update_grid_sprite(player.cart_sprite, grid_index.x, grid_index.y, grid_index.rotation_degrees);
+}
+
+event_handlers.connection = (event) => {
+    client_id = event.client_id;
+};
